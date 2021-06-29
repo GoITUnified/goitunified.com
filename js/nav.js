51fdabb89ee38b794ad98d719719598a6d5d3e85
IT Unified <andrzej.nosal@goitunified.com>
2021-06-29 13:05:26 -0500

    Update

--- js/nav.js
+++ js/nav.js
@@ -17,5 +17,33 @@ $(document).ready(function(){
     });
 });
 
+var tabindex = 1;
+$('input,select').each(function() {
+  if (this.type != "hidden") {
+    var $input = $(this);
+    $input.attr("tabindex", tabindex);
+    tabindex++;
+  }
+});
 
 
+
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})();
